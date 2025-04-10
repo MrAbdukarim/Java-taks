1ca37b7329abee31ec4e9b0668dfe81036dd9a55
MrAbdukarim <abdukarimqarshiyev1@gmail.com>
2025-04-10 13:15:17 +0500

    Add dataTypes.java

---
+++ dataTypes.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+
+public class dataTypes {
+    public static void main(String[] args) {
+//        //Task 1
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Sonni kiriting: ");
+//        int num = scanner.nextInt();
+//
+//        List<Integer> numbers = new ArrayList<>();
+//
+//        for (int i = 1; i <= num; i++) {
+//            if (i % 3 == 0 && i % 5 != 0) {
+//                numbers.add(i);
+//            }
+//        }
+//
+//        System.out.printf("3 ga bo‘linadigan lekin 5 ga bo‘linmaydigan son: %s", numbers);
+
+//  //        Task 2
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Son kiriting: ");
+//        int enter_num = scanner.nextInt();
+//
+//        if (enter_num % 1 == 0 && enter_num % enter_num == 0) {
+//            System.out.println("Bu tub son");
+//        } else {
+//            System.out.println("Bu tub son emas!");
+//        }
+    }
+}
